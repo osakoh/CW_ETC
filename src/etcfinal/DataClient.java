03453ed8bd6f67252422b8cb040e9e74deca0a9a
Michael Osakoh <macmive@gmail.com>
2019-12-07 15:24:06 +0000

    MainMenu method created in DataClient class

--- src/etcfinal/DataClient.java
+++ src/etcfinal/DataClient.java
@@ -5,10 +5,30 @@
  */
 package etcfinal;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author Osas
  */
 public class DataClient {
     
+    
+ 
+        
+    public void mainMenu(){
+        System.out.println("+-------- Extra Tuition Centre (ETC) --------+");
+        System.out.println("| 1.      Book lesson                        |");
+        System.out.println("| 2.      Change lesson                      |");
+        System.out.println("| 3.      Cancel lesson                      |");
+        System.out.println("| 4.      Attend Lesson                      |");
+        System.out.println("| 5.      Retrieve book list                 |");
+        System.out.println("| 6.      Print monthly report               |");
+        System.out.println("| 0.      Quit                               |");
+        System.out.println("+--------------------------------------------+");
+
+    }
 }
